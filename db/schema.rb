7ac32e37c8bc4fb01fee159d6bf97cd9d82218ec
RobertCam <rob.cameron85@gmail.com>
2015-10-19 23:03:05 +0000

    added user session, sign-in, login, and logout

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151019222230) do
+ActiveRecord::Schema.define(version: 20151019225931) do
 
   create_table "tracks", force: :cascade do |t|
     t.string   "title"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20151019222230) do
     t.string   "length"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
   end
 
   create_table "users", force: :cascade do |t|
